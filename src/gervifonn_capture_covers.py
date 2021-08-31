6ea4aeb4122aa4327f654fc825fc2b0aa42ca76d
irishrain <74204581+irishrain@users.noreply.github.com>
2021-08-31 13:18:39 +0200

    Added missing licence text

--- src/gervifonn_capture_covers.py
+++ src/gervifonn_capture_covers.py
@@ -1,5 +1,21 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2021 Fabian Jansen
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import tkinter as tk
-#import cv2
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
